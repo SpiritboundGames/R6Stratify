d25bf197e45f09dd2b104b5eb1e7d8140f793b58
Casey <103451081+dev-syn@users.noreply.github.com>
2024-05-05 02:13:43 -0400

    Add Profiles menu

--- R6Stratify/Views/MainView.axaml.cs
+++ R6Stratify/Views/MainView.axaml.cs
@@ -1,11 +1,14 @@
 using Avalonia.Controls;
 
-namespace R6Stratify.Views;
+namespace R6Stratify.Views {
 
-public partial class MainView : UserControl
-{
-    public MainView()
-    {
-        InitializeComponent();
+    public partial class MainView : UserControl {
+        public MainView() {
+            InitializeComponent();
+        }
+    
+        
     }
+    
+    
 }
