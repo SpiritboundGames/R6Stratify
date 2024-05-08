079ca6aa3f0ad98e26784e25ef0529b9928d16dc
Casey <103451081+dev-syn@users.noreply.github.com>
2024-05-07 20:03:08 -0400

    init

---
+++ R6Stratify/Views/MainMenu.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace R6Stratify.Views {
+
+	public partial class MainMenu : UserControl {
+
+		public MainMenu() { InitializeComponent(); }
+
+	}
+
+}
